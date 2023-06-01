7b6407d305a1d17d09d1cf13528d97a412d01587
Kevinmigisha276l3b <123960627+Kevinmigisha276l3b@users.noreply.github.com>
2023-06-01 18:13:29 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+function mytimer() {
+    alert( "PLEASE LOGIN  FIRST OR SIGN UP");
+}
+function QW() {
+    alert("YOU ARE WELCOMED GET MORE AT SERVICE AND PRODUCT")
+}
+function bill() {
+    alert("THANKS FOR YOUR LOGIN")
+}
